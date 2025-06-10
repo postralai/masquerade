60bb8edb3ff154b4573599935ba72495ebde6039
Peter Todorov <peter.todorov@live.com>
2025-06-10 04:31:31 +0000

    Make the main.py work. Now the code sensities a PDF end-to-end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import json
 from get_pdf_text import get_pdf_text
 from get_sensitive_data import get_sensitive_data
 from assign_new_values import assign_new_value_with_llm
-from swap_text_pdf import swap_text_pdf
+from swap_text_pdf import create_pdfs
 
 PDF_PATH = "ok_org.pdf"
 
@@ -9,10 +10,9 @@ def main():
     text = get_pdf_text(PDF_PATH)
     sensitive_data_json = get_sensitive_data(text)
     sensitive_data = json.loads(sensitive_data_json)
-    old_values = sensitive_data.values()
-    new_values = [assign_new_value_with_llm(value) for value in original_values]
-    swapped_text = swap_text_pdf(text, old_values, new_values)
-    
+    old_values = [item for value in sensitive_data.values() if value and value is not None for item in (value if isinstance(value, list) else [value])]
+    new_values = [assign_new_value_with_llm(value) for value in old_values]
+    create_pdfs(PDF_PATH, old_values, new_values)
 
 if __name__ == "__main__":
     main()
