fe03090a13b2f9e96f8fa5516988f8f766292a77
Peter Todorov <peter.todorov@live.com>
2025-06-21 18:52:19 +0000

    Move 'redact_data.pdf' and 'preudonymize_data.py' to 'scripts/'

--- scripts/redact_data.py
+++ scripts/redact_data.py
@@ -1,9 +1,11 @@
 from masquerade.redact_per_pdf import redact_pdf
+from masquerade.tinfoil_llm import TinfoilLLM
 from pprint import pprint
 
 
 if __name__ == "__main__":
-    redaction_summary = redact_pdf("insurance_offer.pdf")
+    tinfoil_llm = TinfoilLLM()
+    redaction_summary = redact_pdf("insurance_offer.pdf", tinfoil_llm)
     print("\nRedaction Summary:")
     pprint(redaction_summary, indent=2, width=100)
     
